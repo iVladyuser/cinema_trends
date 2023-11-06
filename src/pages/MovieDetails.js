1b8ed8118da64498ac7ce09a13f6da5ff21f0eb2
iVladyuser <korol.v09@gmail.com>
2023-11-06 18:17:15 +0100

    dd

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,8 +1,8 @@
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 
 const MovieDetails = () => {
-  const {dogId} = useParams();
+
 
   useEffect(() => {});
   return <>FilmDetails</>;
